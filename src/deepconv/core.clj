c1d9eb729f884857abcb2e8d11906827f2d3027e
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2011-03-20 11:59:56 -0300

    simple conversions

--- src/deepconv/core.clj
+++ src/deepconv/core.clj
@@ -1,1 +1,14 @@
 (ns deepconv.core)
+
+(deftype Conversion [can-convert? convert])
+(defn can-convert? [conversion value] ((.can-convert? conversion) value))
+(defn convert [conversion value] ((.convert conversion) value))
+
+(defn make-converter [& conversions]
+  (fn [value]
+    (if-let [conversion (some #(and (can-convert? % value) %) conversions)]
+      (convert conversion value)
+      value)))
+
+(defn conversion-for-type [convert-type f]
+  (Conversion. #(= convert-type (type %)) f))
--- test/deepconv/test/core.clj
+++ test/deepconv/test/core.clj
@@ -2,5 +2,11 @@
   (:use [deepconv.core] :reload)
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest simple-conversions
+  (testing "conversion-for-type"
+    (let [int-conv (conversion-for-type java.lang.Integer (partial + 1))
+          double-conv (conversion-for-type java.lang.Double (partial + 2.0))
+          converter (make-converter int-conv double-conv)]
+      (is (= (+ 1 3) (converter 3)))
+      (is (= (+ 2.0 5.0) (converter 5.0))))
+           ))
