5c02477704317ee6bb8116d182f6cac1863ca26d
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2011-03-20 12:24:52 -0300

    nested conversions

--- src/deepconv/core.clj
+++ src/deepconv/core.clj
@@ -2,13 +2,34 @@
 
 (deftype Conversion [can-convert? convert])
 (defn can-convert? [conversion value] ((.can-convert? conversion) value))
-(defn convert [conversion value] ((.convert conversion) value))
+(defn convert [conversion value others] ((.convert conversion) value others))
 
 (defn make-converter [& conversions]
-  (fn [value]
+  (fn conv [value]
     (if-let [conversion (some #(and (can-convert? % value) %) conversions)]
-      (convert conversion value)
+      (convert conversion value conv)
       value)))
 
 (defn conversion-for-type [convert-type f]
-  (Conversion. #(= convert-type (type %)) f))
+  (Conversion. #(= convert-type (type %))
+               (fn [v o]
+                 (f v))))
+
+(defn vector-conversion []
+  (Conversion. vector?
+               (fn [v o]
+                 (vec (map o v)))))
+
+(defn list-conversion []
+  (Conversion. list?
+               (fn [v o]
+                 (list* (map o v)))))
+
+(defn map-conversion []
+  (Conversion. map?
+               (fn [v o]
+                 (reduce
+                   (fn [res [k v]]
+                     (assoc res (o k) (o v)))
+                   {}
+                   v))))
--- test/deepconv/test/core.clj
+++ test/deepconv/test/core.clj
@@ -3,10 +3,44 @@
   (:use [clojure.test]))
 
 (deftest simple-conversions
-  (testing "conversion-for-type"
-    (let [int-conv (conversion-for-type java.lang.Integer (partial + 1))
-          double-conv (conversion-for-type java.lang.Double (partial + 2.0))
-          converter (make-converter int-conv double-conv)]
-      (is (= (+ 1 3) (converter 3)))
-      (is (= (+ 2.0 5.0) (converter 5.0))))
-           ))
+  (let [int-conv (conversion-for-type java.lang.Integer (partial + 1))
+        double-conv (conversion-for-type java.lang.Double (partial + 2.0))]
+
+    (testing "conversion-for-type"
+      (let [converter (make-converter int-conv double-conv)]
+        (is (= (+ 1 3) (converter 3)))
+        (is (= (+ 2.0 5.0) (converter 5.0)))))
+
+    (testing "vector-conversion"
+      (let [converter (make-converter int-conv double-conv (vector-conversion))]
+        (is (= (+ 1 3) (converter 3)))
+        (is (= [(+ 1 3) (+ 2.0 5.0)] (converter [3 5.0])))))
+
+    (testing "list-conversion"
+      (let [converter (make-converter int-conv double-conv (list-conversion))]
+        (is (= (+ 1 3) (converter 3)))
+        (is (= (list (+ 1 3) (+ 2.0 5.0)) (converter (list 3 5.0))))))
+
+    (testing "map-conversion"
+      (let [converter (make-converter int-conv double-conv (map-conversion))]
+        (is (= (+ 1 3) (converter 3)))
+        (is (= {(+ 1 3) (+ 2.0 5.0)} (converter {3 5.0})))))))
+
+(deftest mixed-conversions
+  (let [int-conv (conversion-for-type java.lang.Integer (partial + 1))
+        double-conv (conversion-for-type java.lang.Double (partial + 2.0))
+        conversions [int-conv double-conv (vector-conversion)
+                     (list-conversion) (map-conversion)]
+        converter (apply make-converter conversions)
+        four (+ 1 3)
+        seven (+ 2.0 5.0)]
+    (is (= four (converter 3)))
+    (is (= seven (converter 5.0)))
+    (is (= [four seven] (converter [3 5.0])))
+    (is (= (list four seven) (converter (list 3 5.0))))
+    (is (= {four seven} (converter {3 5.0})))
+    (is (= {four seven} (converter {3 5.0})))
+    (is (= [four [seven] [[(list four)]] [{[] (list seven)}]]
+           (converter [3 [5.0] [[(list 3)]] [{[] (list 5.0)}]])))
+    (is (= {[four seven] {four seven}, four seven}
+           (converter {[3 5.0] {3 5.0}, 3 5.0})))))
