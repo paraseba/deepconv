210a958a4401ec91335fcc1162a8b227f07bf449
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2011-03-20 11:11:17 -0300

    initial commit

---
+++ src/deepconv/core.clj
@@ -0,0 +1,1 @@
+(ns deepconv.core)
---
+++ test/deepconv/test/core.clj
@@ -0,0 +1,6 @@
+(ns deepconv.test.core
+  (:use [deepconv.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
