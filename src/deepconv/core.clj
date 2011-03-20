7e235f003b73dc6118394b6fc0cac5676415e531
Sebastián Bernardo Galkin <sgalkin@grantaire.com.ar>
2011-03-20 12:37:41 -0300

    Nested conversions are values now
    Added documentation

--- src/deepconv/core.clj
+++ src/deepconv/core.clj
@@ -1,31 +1,59 @@
-(ns deepconv.core)
+(ns deepconv.core
+  "Generic conversion of values. Provides conversions based on type, and
+  nested conversions for lists, vectors & maps")
 
+; Implement a generic conversion.
+; can-convert? is a function that takes a value and returns true if it
+; can be handled by this conversion
+; convert implements the actual conversion, it receives the value to
+; convert and a function representing other converters to handle nested values
 (deftype Conversion [can-convert? convert])
-(defn- can-convert? [conversion value] ((.can-convert? conversion) value))
-(defn- convert [conversion value others] ((.convert conversion) value others))
 
-(defn make-converter [& conversions]
+(defn- can-convert?
+  "Calls can-convert? member of a conversion"
+  [conversion value]
+  ((.can-convert? conversion) value))
+
+(defn- convert
+  "Calls convert member of a conversion"
+  [conversion value others]
+  ((.convert conversion) value others))
+
+(defn make-converter
+  "Create a converter function given a series of conversions. The function
+  returned takes a single argument to be converted"
+  [& conversions]
   (fn conv [value]
     (if-let [conversion (some #(and (can-convert? % value) %) conversions)]
       (convert conversion value conv)
       value)))
 
-(defn conversion-for-type [convert-type f]
+(defn conversion-for-type
+  "Creates a conversion based on type iguality.
+  convert-type is the type handled and f is a function that takes a value
+  of type convert-type and returns the converted value"
+  [convert-type f]
   (Conversion. #(= convert-type (type %))
                (fn [v o]
                  (f v))))
 
-(defn vector-conversion []
+(def ^{:doc "Returns a conversion for vectors. All elements are converted by
+            other conversions, a vector is returned"}
+  vector-conversion
   (Conversion. vector?
                (fn [v o]
                  (vec (map o v)))))
 
-(defn list-conversion []
+(def ^{:doc "Returns a conversion for lists. All elements are converted by
+            other conversions, a list is returned"}
+  list-conversion
   (Conversion. list?
                (fn [v o]
                  (list* (map o v)))))
 
-(defn map-conversion []
+(def ^{:doc "Returns a conversion for maps. All elements are converted by
+            other conversions, a map is returned"}
+  map-conversion
   (Conversion. map?
                (fn [v o]
                  (reduce
--- test/deepconv/test/core.clj
+++ test/deepconv/test/core.clj
@@ -12,25 +12,25 @@
         (is (= (+ 2.0 5.0) (convert 5.0)))))
 
     (testing "vector-conversion"
-      (let [convert (make-converter int-conv double-conv (vector-conversion))]
+      (let [convert (make-converter int-conv double-conv vector-conversion)]
         (is (= (+ 1 3) (convert 3)))
         (is (= [(+ 1 3) (+ 2.0 5.0)] (convert [3 5.0])))))
 
     (testing "list-conversion"
-      (let [convert (make-converter int-conv double-conv (list-conversion))]
+      (let [convert (make-converter int-conv double-conv list-conversion)]
         (is (= (+ 1 3) (convert 3)))
         (is (= (list (+ 1 3) (+ 2.0 5.0)) (convert (list 3 5.0))))))
 
     (testing "map-conversion"
-      (let [convert (make-converter int-conv double-conv (map-conversion))]
+      (let [convert (make-converter int-conv double-conv map-conversion)]
         (is (= (+ 1 3) (convert 3)))
         (is (= {(+ 1 3) (+ 2.0 5.0)} (convert {3 5.0})))))))
 
 (deftest mixed-conversions
   (let [int-conv (conversion-for-type java.lang.Integer (partial + 1))
         double-conv (conversion-for-type java.lang.Double (partial + 2.0))
-        conversions [int-conv double-conv (vector-conversion)
-                     (list-conversion) (map-conversion)]
+        conversions [int-conv double-conv
+                     vector-conversion list-conversion map-conversion]
         convert (apply make-converter conversions)
         four (+ 1 3)
         seven (+ 2.0 5.0)]
